89abf8160d0ab34173f9ec72a73c76ba3a043b1a
JuanBlanco <juanfelipeblancot@hotmail.com>
2021-08-19 21:34:08 -0500

    Advanced project

---
+++ app/src/main/java/co/domi/insta_login/MainActivity.java
@@ -0,0 +1,18 @@
+package co.domi.insta_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
